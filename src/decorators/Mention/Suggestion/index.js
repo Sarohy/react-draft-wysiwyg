850bba67186bf2ba7d28e34b2d97c5066bd0b02b
Abdul Rehman Sarohy <abdul.rehman@zweidevs.com>
2021-09-15 12:12:37 +0500

    Test the suggestion

--- src/decorators/Mention/Suggestion/index.js
+++ src/decorators/Mention/Suggestion/index.js
@@ -270,7 +270,7 @@ function getSuggestionComponent() {
                     { 'rdw-suggestion-option-active': index === activeOption }
                   )}
                 >
-                 Hello World {suggestion.text}
+                 Hello World s! {suggestion.text}
                 </span>
               ))}
             </span>
